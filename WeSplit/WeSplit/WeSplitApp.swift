46454cd0cf74eee9c2e1079d3ed58bef31ae61e0
Alex Vu <alexvuster24@gmail.com>
2026-06-16 17:23:17 -0700

    initial commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Alex Vu on 6/16/26.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
